145c26c7757fdfdc2679133a08c5dc11d8bbbeb9
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2020-12-09 19:41:49 +0300

    Update

--- src/test/java/demo/Demo.java
+++ src/test/java/demo/Demo.java
@@ -3,7 +3,6 @@ package demo;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.concurrent.CompletableFuture;
 
 public class Demo {
@@ -11,7 +10,9 @@ public class Demo {
     public static void main(String... args) {
         Arrays.stream(CompletableFuture.class.getDeclaredMethods())
                 .filter(method ->Modifier.isPublic(method.getModifiers()))
-                .sorted(Comparator.comparing(Method::getName))
-                .forEach(method -> System.out.println(method.getName() + " " + Arrays.toString(method.getParameters())));
+                .map(Method::getName)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
     }
 }
