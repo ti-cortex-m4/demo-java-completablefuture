5c4b850458b9f619469d8cf7c397dea3b552cef7
aliakh <liakh.aliaksandr@gmail.com>
2020-12-10 12:07:43 +0300

    Update

--- src/test/java/demo/Demo.java
+++ src/test/java/demo/Demo.java
@@ -3,17 +3,30 @@ package demo;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
 
 public class Demo {
 
     public static void main(String... args) {
-        Arrays.stream(CompletionStage.class.getDeclaredMethods())
-                .filter(method ->Modifier.isPublic(method.getModifiers()))
+        List<String> list = Arrays.stream(CompletionStage.class.getDeclaredMethods())
+                .filter(method -> Modifier.isPublic(method.getModifiers()))
                 .map(Method::getName)
                 .distinct()
                 .sorted()
+                .collect(Collectors.toList());
+
+        list.forEach(System.out::println);
+        System.out.println();
+
+        Arrays.stream(CompletableFuture.class.getDeclaredMethods())
+                .filter(method -> Modifier.isPublic(method.getModifiers()))
+                .map(Method::getName)
+                .distinct()
+                .sorted()
+                .filter(name -> !list.contains(name))
                 .forEach(System.out::println);
     }
 }
