b583678b6979974785bce823d7c8c579a7a5f449
aliakh <liakh.aliaksandr@gmail.com>
2020-12-10 11:58:18 +0300

    Update

--- src/test/java/demo/Demo.java
+++ src/test/java/demo/Demo.java
@@ -4,11 +4,12 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 public class Demo {
 
     public static void main(String... args) {
-        Arrays.stream(CompletableFuture.class.getDeclaredMethods())
+        Arrays.stream(CompletionStage.class.getDeclaredMethods())
                 .filter(method ->Modifier.isPublic(method.getModifiers()))
                 .map(Method::getName)
                 .distinct()
