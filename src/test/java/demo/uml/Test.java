758181085697f94adcf5ef3d7719b19fba2c1121
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2020-12-20 20:21:29 +0300

    Update

--- src/test/java/demo/uml/Test.java
+++ src/test/java/demo/uml/Test.java
@@ -1,6 +1,9 @@
 package demo.uml;
 
 import de.elnarion.util.plantuml.generator.PlantUMLClassDiagramGenerator;
+import de.elnarion.util.plantuml.generator.classdiagram.VisibilityType;
+import de.elnarion.util.plantuml.generator.config.PlantUMLConfig;
+import de.elnarion.util.plantuml.generator.config.PlantUMLConfigBuilder;
 
 import java.io.FileOutputStream;
 import java.io.PrintStream;
@@ -13,15 +16,14 @@ public class Test {
     public static void main(String[] args) throws Exception {
         List<String> scanpackages = new ArrayList<>();
         scanpackages.add("demo.uml.classes");
-        String whitelistRegExp = ".*Future.*";
-        List<String> hideClasses = new ArrayList<>();
-        PlantUMLClassDiagramGenerator generator = new PlantUMLClassDiagramGenerator(
-                    Test.class.getClassLoader(),
-                        whitelistRegExp,
-                        hideClasses,
-                        true,
-                        false,
-                        scanpackages);
+
+        PlantUMLConfig config = new PlantUMLConfigBuilder(scanpackages)
+                .withMaximumMethodVisibility(VisibilityType.PUBLIC)
+                .withClassLoader(Test.class.getClassLoader())
+                .withRemoveFields(true)
+                .build();
+
+        PlantUMLClassDiagramGenerator generator = new PlantUMLClassDiagramGenerator(config);
         String result = generator.generateDiagramText();
 
         try (PrintStream out = new PrintStream(new FileOutputStream("Future.puml"))) {
