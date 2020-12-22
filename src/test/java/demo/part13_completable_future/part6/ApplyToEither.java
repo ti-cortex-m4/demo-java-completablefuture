2172b829ab73d03c8a4830d83c32928746a1af32
aliakh <liakh.aliaksandr@gmail.com>
2020-12-22 12:01:39 +0300

    Update

--- src/test/java/demo/part13_completable_future/part6/ApplyToEither.java
+++ src/test/java/demo/part13_completable_future/part6/ApplyToEither.java
@@ -1,17 +1,21 @@
-package demo._part13_completable_future.part6;
+package demo.part13_completable_future.part6;
 
 import demo.common.Demo1;
+import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import static org.junit.Assert.assertEquals;
+
 // apply result in Function after finishing any future
 public class ApplyToEither extends Demo1 {
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    @Test
+    public void testApplyToEither() throws InterruptedException, ExecutionException {
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> sleepAndGet(1, "parallel1"))
                 .applyToEither(CompletableFuture.supplyAsync(() -> sleepAndGet(2, "parallel2")),
                         String::toUpperCase);
-        logger.info("result: " + future.get());
+        assertEquals("PARALLEL1", future.get());
     }
 }
