7d8c7ea93c7b372600af0e80c061b55f27b49e76
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2020-12-22 19:11:30 +0300

    Update

---
+++ src/test/java/demo/part13_completable_future/part21/NewIncompleteFuture.java
@@ -0,0 +1,25 @@
+package demo.part13_completable_future.part21;
+
+import demo.common.Demo1;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class NewIncompleteFuture extends Demo1 {
+
+    @Test
+    public void testComplete() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> future1 = CompletableFuture.completedFuture("value");
+        assertTrue(future1.isDone());
+        CompletableFuture<String> future2 = future1.newIncompleteFuture();
+        assertFalse(future2.isDone());
+        future2.complete("value");
+        assertTrue(future2.isDone());
+        assertEquals("value", future2.get());
+    }
+}
