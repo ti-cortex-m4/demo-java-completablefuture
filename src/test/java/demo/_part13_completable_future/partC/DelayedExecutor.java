0dc731c7efaf53e464c1add1cd2e490e540d72c7
aliakh <liakh.aliaksandr@gmail.com>
2020-12-23 14:07:33 +0300

    Update

---
+++ src/test/java/demo/_part13_completable_future/partC/DelayedExecutor.java
@@ -0,0 +1,21 @@
+package demo._part13_completable_future.partC;
+
+import demo.common.Demo1;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+
+public class DelayedExecutor extends Demo1 {
+
+    @Test
+    public void testDelayedExecutor() {
+        Executor executor = CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS);
+        System.out.println("begin");
+        executor.execute(() -> System.out.println("Runnable"));
+        System.out.println("end");
+
+        sleep(2);
+    }
+}
