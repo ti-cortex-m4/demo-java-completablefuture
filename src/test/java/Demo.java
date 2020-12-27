38edfcaca5ee89bc6c510b6561cb0cd265b6cc7a
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2020-12-27 15:44:37 +0300

    Update

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,11 +1,11 @@
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.CompletableFuture;
 
 public class Demo {
 
     public static void main(String... args) {
-        long count = Arrays.stream(CompletionStage.class.getDeclaredMethods())
+        long count = Arrays.stream(CompletableFuture.class.getDeclaredMethods())
                 .filter(method -> Modifier.isPublic(method.getModifiers()))
                 .count();
         System.out.println(count);
