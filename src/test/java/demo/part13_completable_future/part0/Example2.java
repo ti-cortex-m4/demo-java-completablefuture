c0877cc16965cd77d0f1cd562b5d300e40268a12
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2021-02-01 20:41:43 +0300

    Update

---
+++ src/test/java/demo/part13_completable_future/part0/Example2.java
@@ -0,0 +1,31 @@
+package demo.part13_completable_future.part0;
+
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.assertEquals;
+
+public class Example2 {
+
+    @Test
+    public void testFuture() throws InterruptedException, ExecutionException {
+
+//        thenApply,
+//        thenCompose
+//        applyToEither
+//        thenCombine
+
+//        thenAccept
+//        acceptEither
+//        thenAcceptBoth
+
+//        thenRun
+//        runAfterEither
+//        runAfterBoth
+    }
+}
