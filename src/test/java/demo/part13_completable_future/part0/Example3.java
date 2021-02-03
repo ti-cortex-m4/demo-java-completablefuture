a23c88ee5f04d6cd2397d9daf78263849e4bb7aa
aliakh <liakh.aliaksandr@gmail.com>
2021-02-03 16:45:23 +0300

    Update

--- src/test/java/demo/part13_completable_future/part0/Example3.java
+++ src/test/java/demo/part13_completable_future/part0/Example3.java
@@ -4,6 +4,8 @@ import demo.common.Demo1;
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -11,17 +13,24 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 public class Example3 extends Demo1 {
 
     @Test
-    public void testThenApply() throws Exception {
+    public void testThenApplyFast() throws Exception {
+        CompletableFuture<Integer> future = supplyAsync(() -> 2)
+                .thenApply(i -> i + 3); // Function<Integer, Integer>
+        assertEquals(5, future.get().intValue());
+    }
+
+    @Test
+    public void testThenApplySlow() throws Exception {
         CompletableFuture<CompletableFuture<Integer>> future1 = supplyAsync(() -> 2)
-                .thenApply(n -> supplyAsync(() -> n + 3));
-        CompletableFuture<Integer> future2 = future1.get();
+                .thenApply(i -> supplyAsync(() -> i + 3)); // Function<Integer, CompletableFuture<Integer>>
+        CompletableFuture<Integer> future2 = future1.get(); // blocking
         assertEquals(5, future2.get().intValue());
     }
 
     @Test
     public void testThenCompose() throws Exception {
         CompletableFuture<Integer> future = supplyAsync(() -> 2)
-                .thenCompose(n -> supplyAsync(() -> n + 3));
+                .thenCompose(i -> supplyAsync(() -> i + 3)); // Function<Integer, CompletionStage<Integer>>
         assertEquals(5, future.get().intValue());
     }
 }
