b718b1dadaaafd7102ab6b77d8c0c09c1e0be359
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2021-02-15 18:53:26 +0300

    Update

--- src/test/java/demo/part13_completable_future/part0/Example2.java
+++ src/test/java/demo/part13_completable_future/part0/Example2.java
@@ -6,41 +6,52 @@ import org.junit.Test;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-
-import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 public class Example2 extends Demo1 {
 
     @Test
     public void testThenApply() throws InterruptedException, ExecutionException {
         CompletableFuture<String> future = supplyAsync(() -> sleepAndGet("single"))
-                .thenApply(s -> "applied: " + s);
-        assertEquals("applied: single", future.get());
+                .thenApply(value -> value.toUpperCase());
+
+        assertEquals("SINGLE", future.get());
     }
 
     @Test
     public void testThenCompose() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future = supplyAsync(() -> sleepAndGet("sequential1"))
+        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet("sequential1"));
+
+        CompletableFuture<String> future = future1
                 .thenCompose(s -> supplyAsync(() -> sleepAndGet("applied: " + s + " sequential2")));
+
         assertEquals("applied: sequential1 sequential2", future.get());
     }
 
     @Test
     public void testApplyToEither() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future = supplyAsync(() -> sleepAndGet(1, "parallel1"))
-                .applyToEither(supplyAsync(() -> sleepAndGet(2, "parallel2")),
-                        s -> "applied first: " + s);
-        assertEquals("applied first: parallel1", future.get());
+        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+
+        CompletableFuture<String> future = future1
+                .applyToEither(future2,
+                        value -> value.toUpperCase());
+
+        assertEquals("PARALLEL1", future.get());
     }
 
     @Test
     public void testThenCombine() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future = supplyAsync(() -> sleepAndGet("parallel1"))
-                .thenCombine(supplyAsync(() -> sleepAndGet("parallel2")),
-                        (s1, s2) -> "applied both: " + s1 + " " + s2);
-        assertEquals("applied both: parallel1 parallel2", future.get());
+        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet("parallel1"));
+        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet("parallel2"));
+
+        CompletableFuture<String> future = future1
+                .thenCombine(future2,
+                        (value1, value2) -> (value1 + " " + value2).toUpperCase());
+
+        assertEquals("PARALLEL1 PARALLEL2", future.get());
     }
 
 
