e3846aaedc92a5cb8f9fe0c8bfe779b899dd5a3a
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2021-02-15 19:18:03 +0300

    Update

--- src/test/java/demo/part13_completable_future/part0/Example2.java
+++ src/test/java/demo/part13_completable_future/part0/Example2.java
@@ -3,7 +3,7 @@ package demo.part13_completable_future.part0;
 import demo.common.Demo1;
 import org.junit.Test;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -14,106 +14,106 @@ public class Example2 extends Demo1 {
 
     @Test
     public void testThenApply() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future = supplyAsync(() -> sleepAndGet("single"))
+        CompletionStage<String> stage = supplyAsync(() -> sleepAndGet("single"))
                 .thenApply(s -> s.toUpperCase());
 
-        assertEquals("SINGLE", future.get());
+        assertEquals("SINGLE", stage.toCompletableFuture().get());
     }
 
     @Test
     public void testThenCompose() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet("sequential1"));
+        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet("sequential1"));
 
-        CompletableFuture<String> future = future1
+        CompletionStage<String> stage = stage1
                 .thenCompose(s -> {
-                    CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(s + " " + "sequential2"));
-                    return future2;
+                    CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(s + " " + "sequential2"));
+                    return stage2;
                 });
 
-        assertEquals("sequential1 sequential2", future.get());
+        assertEquals("sequential1 sequential2", stage.toCompletableFuture().get());
     }
 
     @Test
     public void testApplyToEither() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
-        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
 
-        CompletableFuture<String> future = future1.applyToEither(future2,
+        CompletionStage<String> stage = stage1.applyToEither(stage2,
                 s -> s.toUpperCase());
 
-        assertEquals("PARALLEL1", future.get());
+        assertEquals("PARALLEL1", stage.toCompletableFuture().get());
     }
 
     @Test
     public void testThenCombine() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet("parallel1"));
-        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet("parallel2"));
+        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet("parallel1"));
+        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet("parallel2"));
 
-        CompletableFuture<String> future = future1.thenCombine(future2,
+        CompletionStage<String> stage = stage1.thenCombine(stage2,
                 (s1, s2) -> (s1 + " " + s2).toUpperCase());
 
-        assertEquals("PARALLEL1 PARALLEL2", future.get());
+        assertEquals("PARALLEL1 PARALLEL2", stage.toCompletableFuture().get());
     }
 
 
     @Test
     public void testThenAccept() throws InterruptedException, ExecutionException {
-        CompletableFuture<Void> future = supplyAsync(() -> sleepAndGet("single"))
+        CompletionStage<Void> stage = supplyAsync(() -> sleepAndGet("single"))
                 .thenAccept(s -> logger.info("consumed single: {}", s));
 
-        assertNull(future.get());
+        assertNull(stage.toCompletableFuture().get());
     }
 
     @Test
     public void testAcceptEither() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
-        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
 
-        CompletableFuture<Void> future = future1.acceptEither(future2,
+        CompletionStage<Void> stage = stage1.acceptEither(stage2,
                 s -> logger.info("consumed first: {}", s));
 
-        assertNull(future.get());
+        assertNull(stage.toCompletableFuture().get());
     }
 
     @Test
     public void testThenAcceptBoth() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
-        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
 
-        CompletableFuture<Void> future = future1.thenAcceptBoth(future2,
+        CompletionStage<Void> stage = stage1.thenAcceptBoth(stage2,
                 (s1, s2) -> logger.info("consumed both: {} {}", s1, s2));
 
-        assertNull(future.get());
+        assertNull(stage.toCompletableFuture().get());
     }
 
 
     @Test
     public void testThenRun() throws InterruptedException, ExecutionException {
-        CompletableFuture<Void> future = supplyAsync(() -> sleepAndGet("single"))
+        CompletionStage<Void> stage = supplyAsync(() -> sleepAndGet("single"))
                 .thenRun(() -> logger.info("run after single"));
 
-        assertNull(future.get());
+        assertNull(stage.toCompletableFuture().get());
     }
 
     @Test
     public void testRunAfterEither() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
-        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
 
-        CompletableFuture<Void> future = future1.runAfterEither(future2,
+        CompletionStage<Void> stage = stage1.runAfterEither(stage2,
                 () -> logger.info("run after first"));
 
-        assertNull(future.get());
+        assertNull(stage.toCompletableFuture().get());
     }
 
     @Test
     public void testRunAfterBoth() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
-        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
 
-        CompletableFuture<Void> future = future1.runAfterBoth(future2,
+        CompletionStage<Void> stage = stage1.runAfterBoth(stage2,
                 () -> logger.info("run after both"));
 
-        assertNull(future.get());
+        assertNull(stage.toCompletableFuture().get());
     }
 }
