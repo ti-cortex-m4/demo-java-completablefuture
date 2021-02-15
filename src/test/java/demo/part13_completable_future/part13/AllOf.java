66258525d9a86bf33b3643a62b63bb9c1f0dae51
aliakh <liakh.aliaksandr@gmail.com>
2021-02-15 17:40:11 +0300

    Update

--- src/test/java/demo/part13_completable_future/part13/AllOf.java
+++ src/test/java/demo/part13_completable_future/part13/AllOf.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class AllOf extends Demo1 {
 
@@ -30,5 +31,22 @@ public class AllOf extends Demo1 {
                 .collect(Collectors.joining(" "));
 
         assertEquals("parallel1 parallel2 parallel3", result);
+    }
+
+    @Test
+    public void testRunAfterBoth() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+
+        CompletableFuture<Void> future = future1
+                .runAfterBoth(future2, () -> logger.info("run after both"));
+        future.get();
+
+        String result = Stream.of(future1, future2)
+                .map(CompletableFuture::join)
+                .map(Object::toString)
+                .collect(Collectors.joining(" "));
+
+        assertEquals("parallel1 parallel2", result);
     }
 }
--- src/test/java/demo/part13_completable_future/part13/AnyOf.java
+++ src/test/java/demo/part13_completable_future/part13/AnyOf.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static org.junit.Assert.assertNull;
 
 public class AnyOf extends Demo1 {
 
@@ -18,6 +19,18 @@ public class AnyOf extends Demo1 {
                 supplyAsync(() -> sleepAndGet(2, "parallel2")),
                 supplyAsync(() -> sleepAndGet(3, "parallel3"))
         );
+
+        assertEquals("parallel1", future.get());
+    }
+
+    @Test
+    public void testApplyToEither() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> future1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
+        CompletableFuture<String> future2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
+
+        CompletableFuture<String> future = future1
+                .applyToEither(future2, value -> value);
+
         assertEquals("parallel1", future.get());
     }
 }
