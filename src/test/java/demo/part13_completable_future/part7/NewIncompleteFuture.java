5b1d482c792d378719df8ff591c1a178c5f903df
aliakh <liakh.aliaksandr@gmail.com>
2021-02-16 08:52:24 +0300

    Update

--- src/test/java/demo/part13_completable_future/part7/NewIncompleteFuture.java
+++ src/test/java/demo/part13_completable_future/part7/NewIncompleteFuture.java
@@ -14,12 +14,12 @@ public class NewIncompleteFuture extends Demo1 {
 
     @Test
     public void testComplete() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future1 = CompletableFuture.completedFuture("value");
+        CompletableFuture<String> future1 = CompletableFuture.completedFuture("value1");
         assertTrue(future1.isDone());
         CompletableFuture<String> future2 = future1.newIncompleteFuture();
         assertFalse(future2.isDone());
-        future2.complete("value");
+        future2.complete("value2");
         assertTrue(future2.isDone());
-        assertEquals("value", future2.get());
+        assertEquals("value2", future2.get());
     }
 }
