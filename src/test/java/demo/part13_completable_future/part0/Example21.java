1632486438ed2e8050703ee55f4488a36f4f0221
pl73pl7yfza9ba1n <pl73pl7yfza9ba1n@gmail.com>
2021-02-21 11:19:49 +0300

    Update

--- src/test/java/demo/part13_completable_future/part0/Example21.java
+++ src/test/java/demo/part13_completable_future/part0/Example21.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
+import static java.util.concurrent.CompletableFuture.runAsync;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -69,33 +70,24 @@ public class Example21 extends Demo1 {
 
     @Test
     public void testThenRun() throws InterruptedException, ExecutionException {
-        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet("single"));
-
-        CompletionStage<Void> stage = stage1.thenRun(
-                () -> logger.info("runs after single"));
-
+        CompletionStage<Void> stage = runAsync(() -> logger.info("run 1"))
+                .thenRun(() -> logger.info("runs after single"));
         assertNull(stage.toCompletableFuture().get());
     }
 
     @Test
     public void testRunAfterEither() throws InterruptedException, ExecutionException {
-        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
-        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
-
-        CompletionStage<Void> stage = stage1.runAfterEither(stage2,
+        CompletionStage<Void> stage = runAsync(() -> { sleep(1); logger.info("run 1"); } )
+                .runAfterEither(runAsync(() -> logger.info("run 2")),
                 () -> logger.info("runs after first"));
-
         assertNull(stage.toCompletableFuture().get());
     }
 
     @Test
     public void testRunAfterBoth() throws InterruptedException, ExecutionException {
-        CompletionStage<String> stage1 = supplyAsync(() -> sleepAndGet(1, "parallel1"));
-        CompletionStage<String> stage2 = supplyAsync(() -> sleepAndGet(2, "parallel2"));
-
-        CompletionStage<Void> stage = stage1.runAfterBoth(stage2,
-                () -> logger.info("runs after both"));
-
+        CompletionStage<Void> stage = runAsync(() -> { sleep(1); logger.info("run 1"); } )
+                .runAfterBoth(runAsync(() -> logger.info("run 2")),
+                        () -> logger.info("runs after both"));
         assertNull(stage.toCompletableFuture().get());
     }
 }
