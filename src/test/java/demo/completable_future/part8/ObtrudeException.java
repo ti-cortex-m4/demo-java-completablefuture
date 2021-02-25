190e6275a16ea4348da605e65d07bff9c7830415
aliakh <liakh.aliaksandr@gmail.com>
2021-02-25 17:03:01 +0300

    Update

--- src/test/java/demo/completable_future/part8/ObtrudeException.java
+++ src/test/java/demo/completable_future/part8/ObtrudeException.java
@@ -1,4 +1,4 @@
-package demo.part13_completable_future.part11;
+package demo.completable_future.part8;
 
 import demo.common.Demo1;
 import org.junit.Test;
@@ -10,28 +10,26 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ObtrudeValue extends Demo1 {
+public class ObtrudeException extends Demo1 {
 
     @Test
-    public void testObtrudeValue1() throws InterruptedException, ExecutionException {
+    public void testObtrudeException1() throws InterruptedException, ExecutionException {
         CompletableFuture<String> future = CompletableFuture.completedFuture("value");
         assertTrue(future.isDone());
         assertFalse(future.isCompletedExceptionally());
         assertEquals("value", future.get());
+        future.obtrudeException(new RuntimeException("error"));
         assertTrue(future.isDone());
-        assertFalse(future.isCompletedExceptionally());
-        future.obtrudeValue("value2");
-        assertEquals("value2", future.get());
+        assertTrue(future.isCompletedExceptionally());
     }
 
     @Test
-    public void testObtrudeValue2() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future = CompletableFuture.failedFuture(new RuntimeException("error"));
+    public void testObtrudeException2() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> future = CompletableFuture.failedFuture(new RuntimeException("error1"));
         assertTrue(future.isDone());
         assertTrue(future.isCompletedExceptionally());
-        future.obtrudeValue("value");
+        future.obtrudeException(new RuntimeException("error2"));
         assertTrue(future.isDone());
-        assertFalse(future.isCompletedExceptionally());
-        assertEquals("value", future.get());
+        assertTrue(future.isCompletedExceptionally());
     }
 }
--- src/test/java/demo/completable_future/part8/ObtrudeValue.java
+++ src/test/java/demo/completable_future/part8/ObtrudeValue.java
@@ -1,4 +1,4 @@
-package demo.part13_completable_future.part11;
+package demo.completable_future.part8;
 
 import demo.common.Demo1;
 import org.junit.Test;
@@ -10,26 +10,28 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ObtrudeException extends Demo1 {
+public class ObtrudeValue extends Demo1 {
 
     @Test
-    public void testObtrudeException1() throws InterruptedException, ExecutionException {
+    public void testObtrudeValue1() throws InterruptedException, ExecutionException {
         CompletableFuture<String> future = CompletableFuture.completedFuture("value");
         assertTrue(future.isDone());
         assertFalse(future.isCompletedExceptionally());
         assertEquals("value", future.get());
-        future.obtrudeException(new RuntimeException("error"));
         assertTrue(future.isDone());
-        assertTrue(future.isCompletedExceptionally());
+        assertFalse(future.isCompletedExceptionally());
+        future.obtrudeValue("value2");
+        assertEquals("value2", future.get());
     }
 
     @Test
-    public void testObtrudeException2() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future = CompletableFuture.failedFuture(new RuntimeException("error1"));
+    public void testObtrudeValue2() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> future = CompletableFuture.failedFuture(new RuntimeException("error"));
         assertTrue(future.isDone());
         assertTrue(future.isCompletedExceptionally());
-        future.obtrudeException(new RuntimeException("error2"));
+        future.obtrudeValue("value");
         assertTrue(future.isDone());
-        assertTrue(future.isCompletedExceptionally());
+        assertFalse(future.isCompletedExceptionally());
+        assertEquals("value", future.get());
     }
 }
